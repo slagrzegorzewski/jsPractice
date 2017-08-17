679f053a6fd15e756999bc36e9e758b915dd74b3
Slawomir Grzegorzewski <slagrzegorzewski@gmail.com>
2017-08-17 17:44:11 +0200

    JS - Basics functions examples

--- functions/src/main.js
+++ functions/src/main.js
@@ -24,7 +24,41 @@ document.addEventListener("DOMContentLoaded", function () {
         return numA + numB + numC;
     }
 
+    function letsRock(k) {
+        for(let x = 1; x <= k; x++){
+            console.log(x + "Check this Out!");
+        };
+    }
+
+    let rentCost = (days) => {
+        if(days == 1){
+            console.log("Cost for one day will be: 200 pln");
+        }
+        else if(days > 1 && days < 4){
+            let twoThreeDays = 180 * days;
+            console.log("Visit cost for: " + days + " days, will be: " + twoThreeDays);
+        }
+        else if(days > 3 && days < 8){
+            let fourSevenDays = days * 160;
+            console.log("Visit cost for: " + days + " days, will be: " + fourSevenDays);
+        }
+        else if(days > 7){
+            let evenThanSeven = days * 150,
+                numbersOfWeeks = Math.floor(days / 7),
+                totalCost = evenThanSeven - numbersOfWeeks * 50;
+            console.log("promotion: " + numbersOfWeeks * 50);
+            console.log("Visit cost for: " + days + " days, will be: " + totalCost);
+        }
+    };
+
+    let chckFunction = letsRock(10);
+    console.log(chckFunction);
+
     console.log(mixing("Banana"));
     console.log(moreThan(33.1,33));
     console.log(numSum(3,3,7));
+    rentCost(5);
+    rentCost(1);
+    rentCost(3);
+    rentCost(8);
 })
