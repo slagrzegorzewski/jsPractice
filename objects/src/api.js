a524c0147331bbed744ae250b3155a8e1bdcaa38
Slawomir Grzegorzewski <slagrzegorzewski@gmail.com>
2017-08-23 21:38:27 +0200

    JS - calculator object example

--- objects/src/api.js
+++ objects/src/api.js
@@ -25,4 +25,81 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log(coco);
     leia.play("ball");
     coco.play("rabbit");
+
+    leia.injectionHistory = ["20.09.2016", "17.02.2016", "20.09.2017"];
+    console.log(leia);
+    leia.newInjection = function(date){
+        this.date = date;
+        leia.injectionHistory.push(date);
+        console.log("Next injection: " + date);
+    }
+    leia.newInjection("26.12.2017");
+    console.log(leia.injectionHistory);
+
+    let Calculator = function(){
+        this.numOne;
+        this.numTwo;
+        this.results = [];
+    }
+
+    let scoreCalculator = new Calculator();
+
+    Calculator.prototype.add = function(a,b){
+        this.numOne = a;
+        this.numTwo = b;
+        const solution = a + b;
+        const msg = "Added " + a + " to " + b + " got: " + solution;
+        this.results.push(msg);
+        return msg;
+    };
+    Calculator.prototype.multip = function(a,b){
+        this.numOne = a;
+        this.numTwo = b;
+        const solution = a * b;
+        const msg = "Multiplied " + a + " to " + b + " got: " + solution;
+        this.results.push(msg);
+        return msg;
+    };
+    Calculator.prototype.substract = function(a,b){
+        this.numOne = a;
+        this.numTwo = b;
+        const solution = a - b;
+        const msg = "Substracted " + a + " to " + b + " got: " + solution;
+        this.results.push(msg);
+        return msg;
+    };
+    Calculator.prototype.divide = function(a,b){
+        this.numOne = a;
+        this.numTwo = b;
+        const solution = a / b;
+        const msg = "Divided " + a + " to " + b + " got: " + solution;
+        this.results.push(msg);
+        return msg;
+    };
+    Calculator.prototype.printOperations = function(){
+        console.log(scoreCalculator.results);
+    };
+    Calculator.prototype.clearOperations = function(){
+        let i,
+            arrLength = this.results.length;
+        for(i=0; i<arrLength; i++){
+            if(arrLength === 1){
+                this.results.splice(0);
+            }else{
+                this.results.splice(0,i);
+            }
+        }
+    };
+    let addCalc = scoreCalculator.add(10,10);
+    scoreCalculator.add(23,30);
+    scoreCalculator.multip(4,10);
+    scoreCalculator.substract(33,11);
+    scoreCalculator.substract(33,11);
+    scoreCalculator.substract(33,11);
+    scoreCalculator.divide(33,11);
+    scoreCalculator.divide(33,11);
+    scoreCalculator.divide(33,11);
+    scoreCalculator.printOperations();
+    scoreCalculator.clearOperations();
+    scoreCalculator.printOperations();
 });
